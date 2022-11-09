50bc3a2cc0e1d87ac4201c56a47bd7893fd8de4e
Maxim Uvarov <maxim.uvarov@linaro.org>
2022-11-09 13:11:32 +0000

    add fido_rootfs.sh: for custom fix rootfs
    Signed-off-by: Maxim Uvarov <maxim.uvarov@linaro.org>

---
+++ board/qemu/aarch64-sbsa/fido_rootfs.sh
@@ -0,0 +1,23 @@
+set -eux
+
+echo "fs was stripped with fido_rootfs.sh script" >>  $1/root/fido_rootfs
+
+
+rm -rf $1/lib/modules/*
+
+rm $1/usr/sbin/cryptsetup
+rm $1/usr/sbin/dmeventd
+rm $1/usr/sbin/dmsetup
+rm $1/usr/sbin/integritysetup
+rm $1/usr/sbin/veritysetup
+
+rm -rf $1/usr/share/*
+
+# rm -rf $1/usr/lib/*
+rm -rf $1/usr/lib/libclang-cpp*
+rm -rf $1//usr/lib/libLLVM*
+rm -rf $1//usr/lib/libLTO.so*
+rm -rf $1//usr/lib/libaio.so*
+# rm -rf $1//usr/lib/libdevmapper*
+# rm -rf $1//usr/lib/libjson-c*
+rm -rf $1//usr/lib/libstdc*
