23cc25ea691ef818e77c7550d72fff4fc4c335c1
Maxim Uvarov <maxim.uvarov@linaro.org>
2022-11-11 10:13:02 +0000

    use client.tar.gz for FDO unboarding file
    Signed-off-by: Maxim Uvarov <maxim.uvarov@linaro.org>

--- board/qemu/aarch64-sbsa/fido_rootfs.sh
+++ board/qemu/aarch64-sbsa/fido_rootfs.sh
@@ -11,19 +11,22 @@ rm $1/usr/sbin/dmsetup
 rm $1/usr/sbin/integritysetup
 rm $1/usr/sbin/veritysetup
 
-rm -rf $1/usr/share/*
+#keep share for dhcp
+#rm -rf $1/usr/share/*
 
 # rm -rf $1/usr/lib/*
 rm -rf $1/usr/lib/libclang-cpp*
-rm -rf $1//usr/lib/libLLVM*
-rm -rf $1//usr/lib/libLTO.so*
-rm -rf $1//usr/lib/libaio.so*
+rm -rf $1/usr/lib/libLLVM*
+rm -rf $1/usr/lib/libLTO.so*
+rm -rf $1/usr/lib/libaio.so*
 # rm -rf $1//usr/lib/libdevmapper*
 # rm -rf $1//usr/lib/libjson-c*
 rm -rf $1//usr/lib/libstdc*
 
-tar xpvfz fdo_client_keys.tar.gz --strip-components=1 -C $1
+tar xpvfz client.tar.gz --strip-components=1 -C $1
 
 # fix musl bug
 ln -s /lib64/ld-musl-aarch64.so.1 $1/bin/ldd
 ln -s /lib64/ld-musl-aarch64.so.1 $1/lib/ld-linux-aarch64.so.1
+
+echo export DEVICE_CREDENTIAL=/etc/device_credential >> $1/etc/profile 
